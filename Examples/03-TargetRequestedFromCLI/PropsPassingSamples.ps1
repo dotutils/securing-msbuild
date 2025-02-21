3010022d54c196b26f0617236be05a34766c910e
Jan Krivanek <jankrivanek@microsoft.com>
2025-02-21 15:55:09 +0100

    Adjustments

--- Examples/03-TargetRequestedFromCLI/PropsPassingSamples.ps1
+++ Examples/03-TargetRequestedFromCLI/PropsPassingSamples.ps1
@@ -1,22 +1,36 @@
-$env:prop8=""
+$env:PreBuildEvent=""
 echo "Execute without passing props"
 echo "'dotnet build'"
 dotnet build
 echo "============================="
+echo "Hit key to continue"
 echo ""
+$null = [System.Console]::ReadKey().Key
+echo "Execute with requesting target explicitly"
+echo "'dotnet build -t:MyAfterBuild'"
+dotnet build -t:MyAfterBuild
+echo "============================="
+echo "Hit key to continue"
+echo ""
+$null = [System.Console]::ReadKey().Key
 echo "Execute with passing global props"
 echo "'dotnet build -p:BuildDependsOn=MyAfterBuild'"
 dotnet build -p:BuildDependsOn=MyAfterBuild
 echo "============================="
+echo "Hit key to continue"
 echo ""
+$null = [System.Console]::ReadKey().Key
 echo "Execute with passing global props"
 echo "'dotnet build -p:PreBuildEvent=`"msg %username% BUILD HIJACKED! (PreBuildEvent from CLI)`""
 dotnet build -p:PreBuildEvent="msg %username% BUILD HIJACKED! (PreBuildEvent from CLI)"
 echo "============================="
+echo "Hit key to continue"
 echo ""
+$null = [System.Console]::ReadKey().Key
 echo "Execute with prop from env"
 echo "`$env:PreBuildEvent=`"msg %username% BUILD HIJACKED! (PreBuildEvent from env)`""
 $env:PreBuildEvent="msg %username% BUILD HIJACKED! (PreBuildEvent from env)"
 echo "'dotnet build'"
 dotnet build
 echo "============================="
+$env:PreBuildEvent=""
