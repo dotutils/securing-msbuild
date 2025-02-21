ca6675cecad4d1f87cd61c04e734b003b72f8704
Jan Krivanek <jankrivanek@microsoft.com>
2025-02-21 11:58:35 +0100

    Initial examples and materials

---
+++ Examples/03-TargetRequestedFromCLI/PropsPassingSamples.ps1
@@ -0,0 +1,22 @@
+$env:prop8=""
+echo "Execute without passing props"
+echo "'dotnet build'"
+dotnet build
+echo "============================="
+echo ""
+echo "Execute with passing global props"
+echo "'dotnet build -p:BuildDependsOn=MyAfterBuild'"
+dotnet build -p:BuildDependsOn=MyAfterBuild
+echo "============================="
+echo ""
+echo "Execute with passing global props"
+echo "'dotnet build -p:PreBuildEvent=`"msg %username% BUILD HIJACKED! (PreBuildEvent from CLI)`""
+dotnet build -p:PreBuildEvent="msg %username% BUILD HIJACKED! (PreBuildEvent from CLI)"
+echo "============================="
+echo ""
+echo "Execute with prop from env"
+echo "`$env:PreBuildEvent=`"msg %username% BUILD HIJACKED! (PreBuildEvent from env)`""
+$env:PreBuildEvent="msg %username% BUILD HIJACKED! (PreBuildEvent from env)"
+echo "'dotnet build'"
+dotnet build
+echo "============================="
